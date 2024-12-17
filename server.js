52f2319a8640fc2e6647bcc78ad76906f9295d52
khojiakbarr <chinozbekov09@gmail.com>
2024-12-17 13:15:55 +0500

    upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("./auth");
+
+function isLoggedIn(req, res, next) {
+  req;
+  req.user ? next() : res.sendStatus(401);
+}
+
+const app = express();
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'> Authenticated with google</a>");
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  res.send("Hello! " + req.user.displayName);
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send("Goodbye!");
+});
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate");
+});
+app.listen(4000, () => console.log("listening on", 4000));
